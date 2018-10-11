0e59563122c4078b42fc141e6db7c91a9738f760
Brandon Mellus <mellusbrandon@gmail.com>
2018-10-10 22:23:18 -0400

    Create initial webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+// https://scotch.io/tutorials/setting-up-webpack-for-any-project
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const path = require('path');
+
+require('dotenv').config();
+
+const ENV = process.env.NODE_ENV;
+const isTest = ENV === 'testing';
+const isProd = ENV === 'production';
+
+function setDevTool() {
+  if (isTest) return 'inline-source-map';
+  if (isProd) return 'source-map';
+  return 'eval-source-map';
+}
+
+const config = {
+  entry: path.join(__dirname, '/src/client/js/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'eslint-loader',
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /app.js|node_modules|src\/server/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader' },
+            { loader: 'sass-loader' },
+          ],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin(path.join(__dirname, '/public/css/main.css')),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '/src/client/public/index.html'),
+      inject: 'body',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, '/src/client/public'),
+    port: 5000,
+  },
+  devtool: setDevTool(),
+};
+
+if (isProd) {
+  config.plugins = [
+    ...config.plugins,
+    new UglifyJSPlugin(),
+  ];
+}
+
+module.exports = config;
